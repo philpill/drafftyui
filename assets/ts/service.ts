c5f0fec9ac84f57e38dccd983b95066f239742f2
philpill <github@philpill.net>
2017-11-06 09:36:49 +0000

    abstract out mock service

--- assets/ts/service.ts
+++ assets/ts/service.ts
@@ -1,81 +1,45 @@
 import Game from './game';
 import Piece from './piece';
 import Player from './player';
+import MockService from './service.mock';
 
 export default class Service {
 
-    //temp mock game session
-    tempGames: Game[];
-    tempPlayers: Player[];
+    private mockService: MockService;
 
     constructor() {
 
-        this.tempGames = [];
-        this.tempPlayers = [];
+        this.mockService = new MockService();
     }
 
     login(username: string, password: string) {
 
-
+        return this.mockService.login(username, password);
     }
 
     createGame(): Promise<Game> {
         // get playerId from cookie
-
-        let game = new Game();
-
-        let player = new Player('bob');
-
-        this.tempPlayers.push(player);
-
-        game.player1 = player;
-
-        this.tempGames.push(game);
-
-        return new Promise((resolve, reject) => {
-
-            resolve(game);
-        });
+        return this.mockService.createGame();
     }
 
     startGame(id: string): Game {
 
-        let game = this.tempGames[0];
-
-        if (game.player1 && game.player2) {
-            game.start();
-        }
-
-        return game;
+        return this.mockService.startGame(id);
     }
 
     getPlayer(id: string): Promise<Player> {
 
-        let players = this.tempPlayers.filter((player: Player) => {
-
-            return player.uuid === id;
-        });
-
-        return new Promise((resolve, reject) => {
-
-            resolve(players[0]);
-        });
+        return this.mockService.getPlayer(id);
     }
 
     getGames(): Promise<Game[]> {
 
-        return new Promise((resolve, reject) => {
-
-            resolve(this.tempGames);
-        });
+        return this.mockService.getGames();
     }
 
     joinGame(gameId: string) {
 
-        let player = new Player('sue');
-        this.tempPlayers.push(player);
-
-        this.tempGames[0].player2 = player;
+        return this.mockService.joinGame(gameId);
     }
 
     exitGame() {
@@ -84,29 +48,35 @@ export default class Service {
 
     terminateGame() {
 
-        this.tempGames[0].finish();
+        this.mockService.terminateGame();
     }
 
     movePiece(pieceId: string, position: number): Piece[] {
 
-        // get piece position
-        let currentPosition = this.tempGames[0].state.findIndex((piece: Piece) => {
-            return piece && piece.uuid === pieceId;
+        // this._sendData('/move', { id: pieceId, position: position });
+
+        return this.mockService.movePiece(pieceId, position);
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    private _sendData<T>(url: string, body?: any): Promise<T> {
+
+        return new Promise<T>((resolve, reject) => {
+
+            return fetch(url, {
+
+                method: body ? 'post' : 'get',
+                body: JSON.stringify(body)
+
+            }).then((response) => {
+
+                return resolve(response.json());
+
+            }).catch(function(err) {
+
+                return reject(err);
+            });
         });
-
-        // check position is empty
-        this.tempGames[0].state[position];
-
-        // check move is legal
-
-        // update piece position
-        let piece = this.tempGames[0].state.find((piece: Piece) => {
-            return piece && piece.uuid === pieceId;
-        });
-
-        piece.position = position;
-
-        return this.tempGames[0].state;
     }
 }
 
